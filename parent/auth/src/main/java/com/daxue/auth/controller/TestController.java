d76aa2916390c2bc9e9f409430a51d13656c36fb
daxue0929 <daxue0929@qq.com>
2022-07-23 12:13:23 +0800

    [auth]测试

--- parent/auth/src/main/java/com/daxue/auth/controller/TestController.java
+++ parent/auth/src/main/java/com/daxue/auth/controller/TestController.java
@@ -1,7 +1,24 @@
-package com.daxue.auth.controller;/**
+package com.daxue.auth.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @author daxue0929
  * @date 2022/7/23
-*/
-    
+ */
+
+@RestController
 public class TestController {
+
+
+    //测试nacos配置中心是否生效
+    @Value("${test}")
+    public String value;
+
+    @GetMapping("/test")
+    public String test() {
+        return value;
+    }
 }
