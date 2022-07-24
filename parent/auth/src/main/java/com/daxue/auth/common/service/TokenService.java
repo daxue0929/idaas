8ff62b54effb92defb77d916a742d439a5852761
daxue0929 <daxue0929@qq.com>
2022-07-24 23:52:30 +0800

    [auth] token service init

--- parent/auth/src/main/java/com/daxue/auth/common/service/TokenService.java
+++ parent/auth/src/main/java/com/daxue/auth/common/service/TokenService.java
@@ -1,6 +1,7 @@
 package com.daxue.auth.common.service;
 
 
+import com.google.common.io.BaseEncoding;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
@@ -22,6 +23,8 @@ import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.Map;
 
 
@@ -32,66 +35,86 @@ import java.util.Map;
 @Service
 public class TokenService {
 
-    public String generateRSAToken(Map<String, Object> payload) throws JOSEException {
-//        Set<Map.Entry<String, Object>> entries = payload.entrySet();
-//        for (Map.Entry<String, Object> entry : entries) {
-//            String key = entry.getKey();
-//            Object value = entry.getValue();
-//        }
-        PrivateKey privateKey = getDefaultRSAPrivateKey();
-        RSAPublicKey publicKey = getRSAPublicKey();
-        RSAKey rsaJWK = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
-        JWSSigner signer = new RSASSASigner(rsaJWK);
-        JWSObject jwsObject = new JWSObject(
-                new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(rsaJWK.getKeyID()).build(),
-                new Payload(payload));
-        jwsObject.sign(signer);
-        String token = jwsObject.serialize();
-        return token;
-    }
+    String publicKeyString = "-----BEGIN PUBLIC KEY-----\n" +
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAz5SepSkXjX8wAlQM25QM\n" +
+            "/pJd11wc+jC8UhSHp0skpO5jPxDtQgkVjWi8G8E1Jgw8LxqxLbduS/FqXND1HRyk\n" +
+            "LF0tJSvGN6KOUKRlZQSqEpcieT3t+t89iaf5+RYS7OgSglETGWXa0uShCaLNR2r1\n" +
+            "pqjTozy2ffXxdGEoguu+zxnYjVa+ru905LB2Cc0EbC3sRUSQKmt6tUv7yMghwkeO\n" +
+            "6rAjJncRXU1zAykeyQjJzbJbc7PPg/VDjDtJqHp6fspRcTK3JjAhZkdHKbBr7zwK\n" +
+            "rFmOcDBugTLceQYzr+e6Bex6Yok0MnGp48IOPpH9OtBB/Bo/yBm4bli0SGVt1iua\n" +
+            "ZwIDAQAB\n" +
+            "-----END PUBLIC KEY-----\n";
+    // pkcs8 string
+    String privateKeyString = "-----BEGIN PRIVATE KEY-----\n" +
+            "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDPlJ6lKReNfzAC\n" +
+            "VAzblAz+kl3XXBz6MLxSFIenSySk7mM/EO1CCRWNaLwbwTUmDDwvGrEtt25L8Wpc\n" +
+            "0PUdHKQsXS0lK8Y3oo5QpGVlBKoSlyJ5Pe363z2Jp/n5FhLs6BKCURMZZdrS5KEJ\n" +
+            "os1HavWmqNOjPLZ99fF0YSiC677PGdiNVr6u73TksHYJzQRsLexFRJAqa3q1S/vI\n" +
+            "yCHCR47qsCMmdxFdTXMDKR7JCMnNsltzs8+D9UOMO0moenp+ylFxMrcmMCFmR0cp\n" +
+            "sGvvPAqsWY5wMG6BMtx5BjOv57oF7HpiiTQycanjwg4+kf060EH8Gj/IGbhuWLRI\n" +
+            "ZW3WK5pnAgMBAAECggEAFckFNVQwsprskPH9P3Y0Puthid6S8/b1bK/W5MnQGLck\n" +
+            "F1RRciK97k9VnpwSVeHm1xK4EZI0syes//y2HM4KtNU6jbIgZWpcAT6fnZB1wOm3\n" +
+            "pn3HYhLXxICqjWexqMdaWe7kUEAOTR/NftNlM9+X1FvXikQu6t1K7+nadJMFPXrK\n" +
+            "T2QS/oEsXgO1m+8mY0AxBOqOTH1v3BWocUPXkytyf9xgo+DLGSPuXAlf3pETmmhn\n" +
+            "QvScSx5m+IqadU8cqBhVbKDD66CtHBTfYTZD/4qbVLANMF+w8l3JUyLnJSJz/Ntd\n" +
+            "eApQtNRrKc2e2OeXz3n0Q0Hc9EdNItmJsTGOm2hgcQKBgQD+JKA7UzD1OelBpc+v\n" +
+            "E84LiUpfydN9+35kBW7Lo62ASK/pVEh9HW0y/Gn82QFt9IR3qJVDqTY+ZdJkX4jg\n" +
+            "wLYSOztzjgEQZQ5BRRS0Ynw8t7CFQUGRuEnIBEubl5/OLQDE6enbXdHAtLmWkEuA\n" +
+            "sCZbntgpDNLWIaZSclX/vyu/SwKBgQDRGOYQ7BPZZdf3vCxLDrtHyvSimqq7rs3L\n" +
+            "6U2LKGPNcuwIrlKlcpsdHXX2GDD27TQUpmzPmuzMKwp33G0GFSMnIXzdJmq9dGHp\n" +
+            "J+pa9Ay+RmsuPR5OYOPhbhhg29m1O+A7ondFNc0+yCCi6TczGpC/r0lgRfMWB6pl\n" +
+            "mvuP2Yyz1QKBgQD2aR6uxqeGbUL7csBeeW5jAxeIrpCP6yON6TKW4blc0500N5Md\n" +
+            "NEXnlJNTJDjdmqJBvsQS2WMNtv36ciKuAEvIe4PWM45WjhT3NfqwN3lMCbJuUWo4\n" +
+            "1p07AOcqEiZQ6pN/WWl0V5ADL3duQ7PhL2nRlb7ydkuKcQ/pipBVqs7I5wKBgDRx\n" +
+            "TzqNS0kqLul2a5GDp1sRPxwNOKcL55et8wHk5msf2K0ws4/FV7wMqIpJdPZpdbd8\n" +
+            "KixDigKHBQAc68crw/5LetqoiPrtVqU4mqGnNg7+8GDPeWtlB6KBj2Gzoj2/P7UV\n" +
+            "QV69aLXHUdAqN+r8TbOVzgx0Xsx2rbkADvn8MTrlAoGBAN8dDS4eWHIqb1w9ZFGp\n" +
+            "NXR6eYzuNXY/Evy/N0fupFtwPCQ80x6b26WdC3g1TgypPMRtrKjANo+EEEjB7ZEn\n" +
+            "fOZJklT+jFrGAyaXZBQ4swduFOW13AHBBInMyD9GtlN8o7bY1JJQ9Iy7uC/XR3UU\n" +
+            "lqgYHZSaroGP7fVdjSjq0NNF\n" +
+            "-----END PRIVATE KEY-----\n";
 
-    public RSAPublicKey getRSAPublicKey() {
+
+
+    public String generateRSAToken(Map<String, Object> payload) {
         try {
-            String filePath = this.getClass().getClassLoader().getResource("public_key.pem").getFile();
-
-            InputStreamReader inputStream = new InputStreamReader(new FileInputStream(filePath)); //获取一个输入流
-
-
-            File publicKeyFile = new File(filePath);
-            byte[] keyBytes = new byte[(int) publicKeyFile.length()];
-            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            return (RSAPublicKey) keyFactory.generatePublic(keySpec);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (InvalidKeySpecException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
+            PrivateKey privateKey = getPrivateKey();
+            RSAPublicKey publicKey = getPublicKey();
+            RSAKey rsaJWK = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
+            JWSSigner signer = new RSASSASigner(rsaJWK);
+            JWSObject jwsObject = new JWSObject(
+                    new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(rsaJWK.getKeyID()).build(),
+                    new Payload(payload));
+            jwsObject.sign(signer);
+            String token = jwsObject.serialize();
+            return token;
+        }catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    /**
-     * java.security.spec.InvalidKeySpecException: java.security.InvalidKeyException: invalid key format
-     * 解释：https://www.cnblogs.com/jpfss/p/10206054.html
-     * @return
-     * openssl pkcs8 -topk8 -inform PEM -in private_key.pem -outform pem -nocrypt -out pkcs8.pem
-     */
-    public PrivateKey getDefaultRSAPrivateKey() {
-        try {
-            String filePath = this.getClass().getClassLoader().getResource("pkcs8.pem").getFile();
-            File privateKeyFile = new File(filePath);
-            byte[] keyBytes = new byte[(int) privateKeyFile.length()];
-            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
-            return privateKey;
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (InvalidKeySpecException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public PrivateKey getPrivateKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        String privateKey = privateKeyString.replaceAll(
+                "-----(.*)-----(\r\n?|\n|)([\\s\\S]*)(\r\n?|\n|)-----(.*)-----",
+                "$3"
+        ).replaceAll("\n", "").replaceAll(" ", "");
+        byte[] clear = Base64.getDecoder().decode(privateKey.getBytes());
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(clear);
+        KeyFactory fact = KeyFactory.getInstance("RSA");
+        PrivateKey aPrivate = fact.generatePrivate(keySpec);
+        Arrays.fill(clear, (byte) 0);
+        return aPrivate;
     }
+
+    public RSAPublicKey getPublicKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
+         String publicKey = publicKeyString.replaceAll(
+                 "-----(.*)-----(\r\n?|\n|)([\\s\\S]*)(\r\n?|\n|)-----(.*)-----",
+                 "$3"
+         ).replaceAll("\n", "").replaceAll(" ", "");
+         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+         X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(BaseEncoding.base64().decode(publicKey));
+         RSAPublicKey rsaPublicKey = (RSAPublicKey) keyFactory.generatePublic(publicKeySpec);
+         return rsaPublicKey;
+     }
 }
--- parent/common/src/test/java/com/daxue/commom/CommomApplicationTests.java
+++ parent/common/src/test/java/com/daxue/commom/CommomApplicationTests.java
@@ -1,9 +1,9 @@
 package com.daxue.commom;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class CommomApplicationTests {
 
     @Test
